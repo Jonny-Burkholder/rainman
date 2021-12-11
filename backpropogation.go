414b40caa76570659f4b3eb3f25695cbb7463219
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2021-12-10 20:58:03 -0500

    Create backpropogation.go

---
+++ backpropogation.go
@@ -0,0 +1,4 @@
+package neuralnetwork
+
+//I don't know how this works yet, so I can't write it. Obviously
+func BackPropogate() {}
--- layer.go
+++ layer.go
@@ -2,5 +2,8 @@ package neuralnetwork
 
 //Layer is a two-dimensional array of neurons. I guess. How does this work?
 type Layer struct {
-	Neurons [][]*Neuron
+	Neurons [][]*Neuron //Should I abstract the 2-dimensionality? Like, just decide that a row is so many neurons, instead of using a real matrix?
 }
+
+//Activate does what it says on the tin
+func (l *Layer) Activate()

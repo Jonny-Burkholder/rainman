8f5a5c7cd7bbdb1841ba3408d6207a815ff4868c
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2021-12-11 18:09:54 -0500

    Update vectors.go

--- neuralnetwork/vectors.go
+++ neuralnetwork/vectors.go
@@ -1,9 +1,16 @@
 package neuralnetwork
 
+import "errors"
+
 //This should probably be in a more general calculus package in pkg, but that's for a refactor
 
-//Vector is a line with a length, a slope, and a velocity
-//Maybe this should be an interface? But probably not
+var nilVector = &Vector{}
+var errInsufficientVectors = errors.New("Insufficient number of vectors to perform this operation")
+var errUnmatchedVectorDimensions = errors.New("Dimensions of vectors does not match")
+
+//Vector is an n dimensional line segment representing a movement throughout a real or imagined space
+//It may be overkill to define a new type for what is basically a slice of floats, but I think it'll be
+//worth it for the method wrapping
 type Vector struct {
 	Data []float32
 }
@@ -12,4 +19,67 @@ func NewVector(data ...float32) *Vector {
 	return &Vector{
 		Data: data,
 	}
+}
+
+//checkDimensions takes a slice of vectors and verifies that they are all operating in the same dimension
+func checkDimensions(v []*Vector) (int, error) {
+	l := len(v[0].Data)
+	for i := 0; i < len(v); i++ {
+		if len(v[i].Data) != l {
+			return 0, errUnmatchedVectorDimensions
+		}
+	}
+	return l, nil
+}
+
+//DotProduct multiplies two vectors, returning the dot product
+func DotProduct(vectors ...*Vector) (float32, error) {
+	//check that all vectors are in the same number of dimensions?
+	if len(vectors) < 2 {
+		return 0, errInsufficientVectors
+	}
+
+	l, err := checkDimensions(vectors)
+	if err != nil {
+		return 0, err
+	}
+
+	var res float32
+	for i := 0; i < l; i++ {
+		temp := vectors[0].Data[i]
+		for j := 1; j < len(vectors); j++ {
+			temp *= vectors[j].Data[i]
+		}
+		res += temp
+	}
+	return res, nil
+}
+
+//CrossProduct does some voodoo, I'm sure
+func CrossProduct(vectors ...*Vector) (*Vector, error) {
+	return nilVector, nil
+}
+
+//AddVectors returns the sum of two or more vectors
+func AddVectors(vectors ...*Vector) (*Vector, error) {
+	if len(vectors) < 1 {
+		return nilVector, errInsufficientVectors
+	} else if len(vectors) < 2 {
+		return vectors[0], nil
+	}
+	//check that they're all in the same dimension
+	l, err := checkDimensions(vectors)
+	if err != nil {
+		return nilVector, err
+	}
+	res := make([]float32, l)
+	//Add each dimension together
+	for i := 0; i < l; i++ {
+		for j := 0; j < len(vectors); j++ {
+			res[i] += vectors[j].Data[i]
+		}
+	}
+	return &Vector{
+		Data: res,
+	}, nil
 }

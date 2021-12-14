6b0b8024d3a45be481e1a85900cf8fbc4352df4a
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2021-12-13 21:53:02 -0500

    Update structure

--- neuralnetwork/layer.go
+++ neuralnetwork/layer.go
@@ -1,8 +1,14 @@
 package neuralnetwork
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 //Layer is a two-dimensional array of neurons. I guess. How does this work?
 type Layer struct {
 	Neurons []*Neuron //Should I abstract the 2-dimensionality? Like, just decide that a row is so many neurons, instead of using a real matrix?
+	Bias    float32
 }
 
 //NewLayer takes a size input and returns a new layer of that size
@@ -14,11 +20,20 @@ func NewLayer(lsize, nsize int) *Layer {
 	}
 	return &Layer{
 		Neurons: res,
+		Bias:    rand.Float32(),
 	}
 }
 
 //Activate does what it says on the tin
-func (l *Layer) Activate(inputs []float32) []float32 {
-	//do stuff. I'm tired
-	return []float32{}
+func (l *Layer) Activate(inputs []float32) ([]float32, error) {
+	if len(inputs) != len(l.Neurons) {
+		return []float32{}, fmt.Errorf("Hey, I don't know how to do that yet")
+	}
+
+	res := make([]float32, len(l.Neurons))
+
+	for i := 0; i < len(l.Neurons); i++ {
+		res[i] = inputs[i] * l.Neurons[i].Weight
+	}
+	return res, nil
 }
--- neuralnetwork/neuron.go
+++ neuralnetwork/neuron.go
@@ -13,54 +13,30 @@ const (
 
 var errinvalidBias = errors.New("Bias is invalid")
 
+//So we're going to use Neuron and Synapse interchangeably for now
 type Neuron struct {
-	Synapses   []float32 //we really only need the parameters, right?
-	Parameters []float32
-	Bias       float32
+	Weight float32
 }
 
 //NewNeuron returns a new neuron with random values
 func NewNeuron(length int) *Neuron {
 	rand.Seed(time.Now().Unix())
 
-	s := make([]float32, length)
-	p := make([]float32, length)
-
-	//these numbers will need to be normalized to numbers between 0 and 1. I think?
-	for i := 0; i < length; i++ {
-		s[i] = rand.Float32()
-		p[i] = rand.Float32()
-	}
 	return &Neuron{
-		Synapses:   s,
-		Parameters: p,
-		Bias:       rand.Float32(),
+		Weight: rand.Float32(),
 	}
 }
 
-//reBias changes the Bias for an individual neuron
-func (n *Neuron) reBias(w float32) error {
+//reWeight changes the weight for an individual neuron
+func (n *Neuron) reWeight(w float32) error {
 	if w < MinBias || w > MaxBias {
 		return errinvalidBias
 	}
-	n.Bias = w
+	n.Weight = w
 	return nil
 }
 
-//Activate takes an input, runs it through the neuron, and spits out an output
-//This probably shouldn't be done at the neuron level, but what the heck, I'll
-//Make it better in a refactor down the road
-func (n *Neuron) Activate(a []float32) float32 {
-	//first, we create a variable to hold the result
-	var res float32
-	//next, we multiply each input by its corresponding parameter, or weight, and
-	//add that to the res
-	l := len(n.Parameters)
-	for i := 0; i < len(a); i++ {
-		if i >= l { //this is not the right way to handle this. Data should be compressed to fit NN size
-			break
-		}
-		res += a[i] * n.Parameters[i]
-	}
-	return res + n.Bias
+//Fire takes an input, runs it through the neuron, and spits out an output
+func (n *Neuron) Fire(a float32) float32 {
+	return a * n.Weight
 }

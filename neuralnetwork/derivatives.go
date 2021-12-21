45cef80b8cfec2d84fe755b889e1da6d13d482a0
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2021-12-21 13:03:57 -0500

    Create derivatives.go

---
+++ neuralnetwork/derivatives.go
@@ -0,0 +1,31 @@
+package neuralnetwork
+
+//SigmoidPrime returns the derivative for the
+//logistic sigmoid activation function
+func SigmoidPrime() {}
+
+//ReluPrime returns the derivative for the
+//relu activation function
+func ReluPrime() {}
+
+//LinearPrime is probably unnecessary, yeah?
+
+//BinaryStepPrime returns the derivative of
+//the binary step activation function
+func BinaryStepPrime() {}
+
+//TanhPrime returns the derivative of the
+//hyperbolic tangent activation function
+func TanhPrime() {}
+
+//ArcTanPrime returns the derivative of the
+//ArcTan activation function
+func ArcTanPrime() {}
+
+//LeakyReluPrime returns the derivative of
+//the leaky relu activation function
+func LeakyReluPrime() {}
+
+//SwishPrime returns the derivative of the
+//swish activation function
+func SwishPrime() {}

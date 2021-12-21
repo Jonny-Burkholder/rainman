21c20326e5add0a8af4a7b70aa46a2112ac2069f
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2021-12-21 12:25:55 -0500

    Update activation.go
    Implemented several of the most common activation functions. Next I'll be implementing their derivatives for backpropogation

--- neuralnetwork/activation.go
+++ neuralnetwork/activation.go
@@ -1,14 +1,22 @@
 package neuralnetwork
 
+import "math"
+
 //Activation will handle the different activation types
 
-//Sigmoid processes based on a sigmoid function
+//Sigmoid takes an input a and applies the logistic sigmoid
+//function, returning an activation between 0 and 1
 func Sigmoid(a float64) float64 {
-	return a
+	return math.Exp(a) / (math.Exp(a) + 1)
 }
 
-//Relu uses a relu function to determine activation
+//Relu is a nonlinear activation function that takes an input
+//a, and returns a if the input is greater than zero. Otherwise
+//the function returns 0
 func Relu(a float64) float64 {
+	if a < 0 {
+		return 0
+	}
 	return a
 }
 
@@ -17,24 +25,41 @@ func Linear(a float64) float64 {
 	return a
 }
 
-//Binary step uses a binary activation function
+//Binary step takes an input a and returns 1 if a is greater
+//than zero. Otherwise the function returns 0
 func BinaryStep(a float64) float64 {
-	return a
+	if a < 0 {
+		return 0
+	}
+	return 1
 }
 
-//Tahn
-func Tahn(a float64) float64 {
-	return a
+//Tanh takes an input, a , and maps it to the hyperbolic
+//tangent function, producing a probability in the range
+//-1, 1
+func Tanh(a float64) float64 {
+	return math.Tanh(a)
 }
 
-//LeakyRelu
+//ArcTan takes an input a, and returns the arctangent
+//value
+func ArcTan(a float64) float64 {
+	return 1 / (1 + math.Exp(-a)) //doesn't make sense to me either
+}
+
+//LeakyRelu takes an input a, and returns a if a
+//is greater than zero. Otherwise, the function
+//returns 1/1000 the input value
 func LeakyRelu(a float64) float64 {
+	if a < 0 {
+		return .001 * a
+	}
 	return a
 }
 
-//Swish
+//Swish is weird, but probably really good?
 func Swish(a float64) float64 {
-	return a
+	return a * Sigmoid(a)
 }
 
 //Elu

e046850e4926b9b9b86c1623b3fa20a01666e5a6
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2021-12-20 19:33:25 -0500

    Update layer.go

--- neuralnetwork/layer.go
+++ neuralnetwork/layer.go
@@ -2,6 +2,7 @@ package neuralnetwork
 
 import (
 	"math/rand"
+	"time"
 )
 
 //Layer is a two-dimensional array of neurons. I guess. How does this work?
@@ -10,27 +11,46 @@ type Layer struct {
 	Weights [][]float64 //Matrix of connectins to next layer
 }
 
-//NewLayer takes a size input and returns a new layer of that size
-func NewLayer(lsize, nsize int) *Layer {
-	//Should have already caught size errors before this point, so this should be pretty easy in theory
-	res := make([]*Neuron, lsize)
-	for i := 0; i > lsize; i++ {
-		res[i] = NewNeuron(nsize)
+//NewLayer takes two inputs, nsize and wsize, where nsize is the number of neurons
+//in the layer, and wsize is the number of weights for each neuron, aka the number
+//of neurons in the next layer. The function returns a new layer
+func NewLayer(nsize, wsize int) *Layer {
+
+	//activation type should be handled here, not at the neuron level
+
+	rand.Seed(time.Now().UnixNano())
+
+	n := make([]*Neuron, nsize)
+	for i := 0; i < nsize; i++ {
+		n[i] = NewNeuron()
 	}
+
+	//each row of the weights matrix will represent a neuron in the next layer
+	w := make([][]float64, wsize)
+	for i := 0; i < wsize; i++ {
+		//and each column in each row will represent a neuron in the current layer's
+		//relationship to the output neuron
+		w[i] = make([]float64, nsize)
+		for j := 0; j < nsize; j++ {
+			//rando-fill those weights
+			w[i][j] = rand.Float64()
+		}
+	}
+
 	return &Layer{
-		Neurons: res,
-		Bias:    rand.Float64(),
+		Neurons: n,
+		Weights: w,
 	}
+
 }
 
 //Activate does a nasty matrix multiplication thing
-func (l *Layer) Activate(inputs []float32) []float32 {
-
-	res := make([]float32, len(l.Neurons))
-
-	for i := 0; i < len(l.Neurons); i++ {
-		res[i] = inputs[i] * l.Neurons[i].Weight
-	}
+//Evemtially I'll bring in an optimized matrix library,
+//but for now I'll hand-roll it
+func (l *Layer) Activate(inputs []float64) []float64 {
+	var res []float64
+	//Send each input through its respective neuron
+	//Do matrix multiplication to get some vector
 	return res
 }
 

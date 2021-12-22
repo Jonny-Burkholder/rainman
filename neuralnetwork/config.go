912143815400e70f741e021b33e4a60593864f51
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2021-12-22 13:09:03 -0500

    Update config.go

--- neuralnetwork/config.go
+++ neuralnetwork/config.go
@@ -4,15 +4,17 @@ type Config struct {
 	ActivationType int //we'll have a slice of activation types somewhere. I mean really the only point of having them though is for human benefit, a simple int works just fine
 	LearningRate   float64
 	BaseStepSize   float64
+	ReluLeak       float64
 	MaxSteps       int
 	Stochastic     int
 }
 
-func NewConfig(a, m, s int, l, b float64) *Config {
+func NewConfig(a, m, s int, l, b, r float64) *Config {
 	return &Config{
 		ActivationType: a,
 		LearningRate:   l,
 		BaseStepSize:   b,
+		ReluLeak:       r,
 		MaxSteps:       m,
 		Stochastic:     s,
 	}

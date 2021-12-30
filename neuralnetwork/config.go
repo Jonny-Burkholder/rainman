2fa657cfbf1474151891e7e6f777f8129b10920d
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2021-12-30 12:25:48 -0500

    Update config.go

--- neuralnetwork/config.go
+++ neuralnetwork/config.go
@@ -1,8 +1,11 @@
 package neuralnetwork
 
+var costFunctions = []string{"Mean Squared"} //for human reference
+
 type Config struct {
 	ActivationType       int //we'll have a slice of activation types somewhere. I mean really the only point of having them though is for human benefit, a simple int works just fine
 	OutputActivationType int //default will be the same as the main activation type
+	CostFunction         int
 	MaxSteps             int
 	Stochastic           int
 	LearningRate         float64
@@ -10,10 +13,11 @@ type Config struct {
 	ReluLeak             float64
 }
 
-func NewConfig(a, o, m, s int, l, b, r float64) *Config {
+func NewConfig(a, o, c, m, s int, l, b, r float64) *Config {
 	return &Config{
 		ActivationType:       a,
 		OutputActivationType: o,
+		CostFunction:         c,
 		MaxSteps:             m,
 		Stochastic:           s,
 		LearningRate:         l,
@@ -26,6 +30,7 @@ func NewDefaultConfig() *Config {
 	return &Config{
 		ActivationType:       0,
 		OutputActivationType: 0,
+		CostFunction:         0,
 		MaxSteps:             100, //no idea if that's a good size
 		Stochastic:           100,
 		LearningRate:         .001,
